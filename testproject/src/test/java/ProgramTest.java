8d909716fa34f31fd5e50af61adf510ec2aef1b4
Yahaya Bathily <yahayad.dev@gmail.com>
2025-05-22 22:06:27 +0200

    add tests

--- testproject/src/test/java/ProgramTest.java
+++ testproject/src/test/java/ProgramTest.java
@@ -1,3 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import yahvya.Program;
+
 public class ProgramTest {
-    
+    @Test
+    public void testGetName(){
+        assertEquals(new Program().getName(),"name");
+    }
+
+    @Test
+    public void testGetNameFail(){
+        assertEquals(new Program().getName(),"false");
+    }
 }
